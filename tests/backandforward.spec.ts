6befa3539eca2819e4612d7d416264345043a897
Nancy <er.nancybhardwaj@gmail.com>
2025-12-10 12:50:12 +0530

    Added Back and Forward Browser Test

---
+++ tests/backandforward.spec.ts
@@ -0,0 +1,17 @@
+import { test } from '@playwright/test';
+
+test('Broswer Back & Forward Test', async ({ page }) => {
+    await page.goto("https://www.amazon.in/");
+    let title: string = await page.title();
+    console.log('Amazon Title is:' + title);
+    await page.goto("https://www.saucedemo.com/");
+    let titleSwagLabs: String = await page.title();
+    console.log("SWag Labs Title is:" + titleSwagLabs);
+    await page.goBack();
+    console.log(await page.title());
+    await page.goForward();
+    console.log(await page.title());
+    await page.goBack();
+    await page.waitForTimeout(5000);
+}
+)
